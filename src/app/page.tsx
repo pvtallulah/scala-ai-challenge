43d83c3808224a3142d3080942cea276abf65927
pvtallulah <maikinahara@gmail.com>
2024-11-02 17:49:49 -0300

    feat: wire all previous changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-// import { Box } from "./components/Box";
 import useSWR from "swr";
 import { fetcher } from "@/lib";
 import { FramesData } from "@/types";
 import { Points } from "./components/Points";
-import { extend } from "@react-three/fiber";
-import { OrbitControls, TransformControls } from "three-stdlib";
-import { CameraController } from "./components/CameraController";
 import { Cuboids } from "./components/Cuboids";
-extend({ OrbitControls, TransformControls });
+import { OrbitControls } from "@react-three/drei";
 
 export default function Home() {
   const { data, error, isLoading } = useSWR<FramesData>("/api/data", fetcher);
@@ -17,18 +13,13 @@ export default function Home() {
   if (isLoading) return <div className="text-white">Loading...</div>;
   if (error) return <div className="text-red-500">Failed to load data</div>;
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <CameraController />
-      <spotLight
-        position={[10, 10, 10]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
+    <Canvas camera={{ position: [5, 0, 0], near: 0.025 }}>
+      <ambientLight intensity={0.5} />
+      <OrbitControls />
       <Points points={data?.points || []} />
       <Cuboids cuboids={data?.cuboids || []} />
+      <gridHelper args={[50]} />
+      <axesHelper args={[50]} />
     </Canvas>
   );
 }
