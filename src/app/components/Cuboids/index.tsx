4ec6aaac48f9ff438f197c84cd3b24f00a1a4d99
pvtallulah <maikinahara@gmail.com>
2024-11-02 17:49:22 -0300

    feat(cuboids): implement instancemesh and fix render issues

--- src/app/components/Cuboids/index.tsx
+++ src/app/components/Cuboids/index.tsx
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { Cuboid as CuboidType } from "@/types";
-import { useMemo, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { getColorFromZ } from "@/lib";
 
 type Params = {
@@ -11,44 +11,70 @@ export const Cuboids = ({ cuboids }: Params) => {
   const meshRef = useRef<THREE.InstancedMesh>(null);
   const numCuboids = cuboids.length;
 
-  useMemo(() => {
+  useEffect(() => {
     if (!meshRef.current) return;
 
     const colorArray = new Float32Array(numCuboids * 3);
-    const matrix = new THREE.Matrix4();
+    const cube = new THREE.Object3D();
 
     for (let i = 0; i < numCuboids; i++) {
       const cuboid = cuboids[i];
-      const posX = cuboid["position.x"];
-      const posY = cuboid["position.y"];
-      const posZ = cuboid["position.z"];
+
+      const x = cuboid["position.x"];
+      const y = cuboid["position.y"];
+      const z = cuboid["position.z"];
       const dimX = cuboid["dimensions.x"];
       const dimY = cuboid["dimensions.y"];
       const dimZ = cuboid["dimensions.z"];
-      const yaw = cuboid.yaw;
+      const yaw = cuboid["yaw"];
 
-      matrix
-        .identity()
-        .makeRotationY(yaw)
-        .scale(new THREE.Vector3(dimX, dimY, dimZ))
-        .setPosition(posX, posY, posZ);
+      cube.position.set(y, z, x);
+      cube.rotation.set(0, yaw, 0);
+      cube.scale.set(dimY, dimZ, dimX);
+      cube.updateMatrix();
 
-      meshRef.current.setMatrixAt(i, matrix);
+      meshRef.current.setMatrixAt(i, cube.matrix);
 
-      const color = getColorFromZ(posZ);
+      const color = getColorFromZ(z);
       color.toArray(colorArray, i * 3);
     }
-    console.log("Cuboids updateds");
-    meshRef.current.geometry.setAttribute(
-      "color",
-      new THREE.InstancedBufferAttribute(colorArray, 3)
-    );
+
+    const colorAttribute = new THREE.InstancedBufferAttribute(colorArray, 3);
+    meshRef.current.geometry.setAttribute("color", colorAttribute);
+    meshRef.current.instanceMatrix.needsUpdate = true;
   }, [cuboids, numCuboids]);
 
   return (
-    <instancedMesh ref={meshRef} args={[undefined, undefined, numCuboids]}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial vertexColors opacity={0.5} transparent />
-    </instancedMesh>
+    <>
+      <instancedMesh ref={meshRef} args={[undefined, undefined, numCuboids]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial vertexColors opacity={0.45} transparent />
+      </instancedMesh>
+
+      {cuboids.map((cuboid, index) => {
+        const x = cuboid["position.x"];
+        const y = cuboid["position.y"];
+        const z = cuboid["position.z"];
+        const dimX = cuboid["dimensions.x"];
+        const dimY = cuboid["dimensions.y"];
+        const dimZ = cuboid["dimensions.z"];
+        const yaw = cuboid["yaw"];
+
+        return (
+          <lineSegments
+            key={cuboid.uuid || index}
+            position={[y, z, x]}
+            rotation={[0, yaw, 0]}
+            scale={[dimY, dimZ, dimX]}
+          >
+            <edgesGeometry
+              attach="geometry"
+              args={[new THREE.BoxGeometry(1, 1, 1)]}
+            />
+            <lineBasicMaterial attach="material" color="black" />
+          </lineSegments>
+        );
+      })}
+    </>
   );
 };
